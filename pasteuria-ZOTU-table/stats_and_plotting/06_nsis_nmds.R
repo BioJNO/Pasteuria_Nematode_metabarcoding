7ec6961586b1e9c4e467b72cb41c1caf64730121
BioJNO <JamieNeilOrr@gmail.com>
2019-10-01 15:19:46 +0100

    changing repo structure

--- pasteuria-ZOTU-table/stats_and_plotting/06_nsis_nmds.R
+++ pasteuria-ZOTU-table/stats_and_plotting/06_nsis_nmds.R
@@ -5,88 +5,97 @@ library(plyr)
 library(ggplot2)
 library(ggrepel)
 library(grid)
+library(caret)
+library(tidyr)
 
-# Prepare data ---------------------------------------------------------------
-data <- read.csv("pas-zotus-merged-with-meta.csv",
-                 header =T,
-                 na.strings = c("", " ", "NA"))
-# Pull out NSIS sample group
-nsis_data <- data[grep("NSIS", data$group),]
-colnames(nsis_data)
-
-# Drop zotus with a total abundance below 10
-drop <- c(rep(TRUE, 12), colSums(nsis_data[,13:165]) > 10)
-nsis_data <- nsis_data[,drop]
-
-# Load in nsis meta data master sheet
-meta <- read.csv("NSIS-meta-master-sheet.csv")
-colnames(meta)
-meta <- meta[,1:270]
-colnames(meta)
-
-# All samples (including blanks) merged with NSIS soil metadata.
-merged_zero                 <- merge(nsis_data, meta, by = "sample")
-colnames(merged_zero)
-write.csv(merged_zero, "zero_nsis_master_merged.csv")
-
-# Some manual database sorting /trimming of uniformative/incomplete columns.
-cleaned_merged <- read.csv("zero_nsis_master_merged.csv")
-
-# Set up so that if upstream filtering is changed, manual database cleanup
-# does not need to be re-done.
-colnames(cleaned_merged)
-clean_merge_meta <- cleaned_merged[,c(1,47:139)]
-colnames(merged_zero)
-raw_merge_zotu <- merged_zero[, c(1,3:101)]
-final_merge <- merge(raw_merge_zotu, clean_merge_meta, by = "sample")
-colnames(final_merge)
-write.csv(final_merge, "final_merged_NSIS_ZOTU_table.csv")
+## Prepare data ---------------------------------------------------------------
+#data <- read.csv("pas-zotus-merged-with-meta.csv",
+#                 header =T,
+#                 na.strings = c("", " ", "NA"))
+## Pull out NSIS sample group
+#nsis_data <- data[grep("NSIS", data$group),]
+#colnames(nsis_data)
+#
+## Drop zotus with a total abundance below 10 in this sample set
+#drop <- c(rep(TRUE, 12), colSums(nsis_data[,13:136]) > 10, rep(TRUE, 2))
+#nsis_data <- nsis_data[,drop]
+#colnames(nsis_data)
+#
+## Load in nsis meta data master sheet
+#meta <- read.csv("NSIS-meta-master-sheet.csv")
+#colnames(meta)
+#meta <- meta[,1:270]
+#colnames(meta)
+#
+## All samples (including blanks) merged with NSIS soil metadata.
+#merged_zero <- merge(nsis_data, meta, by = "sample")
+#colnames(merged_zero)
+#write.csv(merged_zero, "zero_nsis_master_merged.csv")
+#
+## Some manual database sorting /trimming of uniformative/incomplete columns.
+#clean_merged <- merged_zero[,-c(112:115,118:120,122:128,136,141,144:146,148,
+#                                 150,151,154:159,163:166,168:173,177,180:245,
+#                                 247:256,258:263,265:269, 271:276, 320:353,355:379)]
+#colnames(clean_merged)
+#write.csv(clean_merged, "clean_merged_nsis.csv")
+clean_merged <- read.csv("newvsold_nsis_meta.csv")
+colnames(clean_merged)
+clean_merged <- clean_merged[,-c(1,4)]
 
 # Check the class of all merged variables is correct.
-lapply(final_merge, class)
-colnames(final_merge)
+lapply(clean_merged, class)
+colnames(clean_merged)
 
-# Exclude samples with no Pasteuria.
-final_merge$total <- rowSums(final_merge[,13:100])
-final_merge$perul <- final_merge$total/final_merge$vol
-non_zero <- final_merge[final_merge$perul > 10, ]
+# remove empty samples.
+# To acccount for background noise (we don't want to plot pasteuria where it wasn't really present)
+# set the perul abundance > 0
+non_zero <- subset(clean_merged, perul > 10)
+#write.csv(non_zero, "NSIS_clean_merged_non_zero.csv")
 
-# Subset main pit samples (with the most complete metadata).
-main_pit_non_zero <- non_zero[grep("V0", non_zero$OBSERVATION.NO), ]
-
-# To look at main pit variables only substitute all instances of the
-# "non-zero" data frame in the following lines with the
-# "main_pit_non_zero" data subset generated above.
-
-# Remove columns with missing values.
+# Some samples have incomplete metadata which envfit does not like.
+# Restrict soil metadata to variables which are complete for all samples.
 complete_cols_non_zero <- non_zero[,!sapply(non_zero, function(x) any(is.na(x)))]
 
 # Convert all ZOTUs to per ul.
 colnames(complete_cols_non_zero)
 perul_non_zero <- complete_cols_non_zero
-perul_non_zero[,11:99] <- complete_cols_non_zero[,11:99]/complete_cols_non_zero$vol
+perul_non_zero[,10:106] <- complete_cols_non_zero[,10:106]/complete_cols_non_zero$vol
 
 # Split zotus and metadata.
-colnames(perul_non_zero)    
-
-non_zero_zotu <- perul_non_zero[,11:99]
-non_zero_zotu$total <- rowSums(non_zero_zotu)
-non_zero_meta <- complete_cols_non_zero[,100:148]
-non_zero_meta$total <- non_zero_zotu$total
-non_zero_zotu <- non_zero_zotu[,1:89]
+colnames(perul_non_zero)
+non_zero_zotu <- perul_non_zero[,10:106]
+non_zero_meta <- complete_cols_non_zero[,c(107:125)]
+non_zero_meta$total <- non_zero$abundance
+colnames(non_zero_meta)
+# remove names and NGRs, mineral/organic, and total reads
+non_zero_meta_reduced <- non_zero_meta[,-c(1:5,11,20)]
+# double check class
+lapply(non_zero_meta_reduced, class)
 
 # Assign factors dummy numerical values.
-non_zero_meta_dummy <- dummy.data.frame(non_zero_meta,
+non_zero_meta_dummy <- dummy.data.frame(non_zero_meta_reduced,
                                         names = NULL,
                                         omit.constants = TRUE,
                                         dummy.classes = getOption("dummy.classes"))
+colnames(non_zero_meta_dummy)
+
+# check for independence of metadata variables.
+# using caret generate a correlation matrix of variables
+meta_cor <- cor(non_zero_meta_dummy, method = "spearman")
+highCorr <- sum(abs(meta_cor[upper.tri(meta_cor)]) > .75)
+highCorr
+correlations <- as.data.frame(meta_cor)
+#write.csv(correlations, "NSIS_complete_col_metadata_spearman_correlations.csv")
 
 # Run NMDS and store as data frame -------------------------------------------
+set.seed(8)
+colnames(non_zero_zotu)
 NMDS.log <- log1p(non_zero_zotu)
-sol <- metaMDS(NMDS.log, k=3, trymax = 100)
+sol <- metaMDS(NMDS.log, k=2, trymax = 100)
 scrs <- as.data.frame(scores(sol, display = "sites"))
 # Make sure to note down 
 sol
+# stress = 0.2124701
 # Quck check of NMDS.
 plot(sol)
 
@@ -112,15 +121,20 @@ non_zero_ef_df$p.adj <- p.adjust(non_zero_ef_df$p,
                                  n = length(non_zero_ef_df$p))
 
 # Write envfit dataframes to file.
-write.csv(non_zero_ef_df, "non_zero_ef.csv")
+#write.csv(non_zero_ef_df, "NMDS_soil_envfit.csv")
 
 # For point sizing.
 scrs <- cbind(scrs, total.amplicons = non_zero_meta$total)
-scrs <- cbind(scrs, volume = non_zero_meta$vol)
 
 # Significant variables after correction for false discovery.
+sig_meta <- subset(non_zero_ef_df, p.adj<0.05)
+
+scrs <- cbind(scrs, carbon = non_zero_meta_dummy$NSIS2.CARBON)
 scrs <- cbind(scrs, record.type = non_zero_meta$RECORD.TYPE)
+scrs <- cbind(scrs, ph = non_zero_meta$NSIS2.PH.H20)
+scrs <- cbind(scrs, density = non_zero_meta$NSIS2.DBD.AVG.FINE.EARTH.ONLY)
 scrs <- cbind(scrs, horizon = non_zero_meta$Horizon)
+scrs <- cbind(scrs, perul = non_zero_meta$perul)
 
 # Fit ZOTUs to NMDS plot.
 vf <- envfit(sol, NMDS.log, perm=999)
@@ -139,63 +153,44 @@ spp.scrs$p.adj <- p.adjust(spp.scrs$p,
                            n = length(spp.scrs$p))
 
 # Write out species fits.
-write.csv(spp.scrs, "non_zero_species_fit.csv")
-
+#write.csv(spp.scrs, "NSIS_species_ordination_fit.csv")
 all_fits <- rbind(spp.scrs, non_zero_ef_df)
  
 # Create subgroup of pval cuttoffs.
 adjsig <- subset(all_fits, p.adj<0.05)
+adjsig$Species
+adjsig$SPecies.sensible <- c("X26_Pasteuria.hartismeri_0.99",
+                             "X4_Pasteuria.luffness_0.98",
+                             "X441_Pasteuria_0.95",
+                             "Horizon A",
+                             "Dry Bulk Density",
+                             "Carbon",
+                             "pH",
+                             "Moisture")
+
+# store a colourblind pallete
+cb = c("#D81B60", "#1E88E5", "#FFC107", "#004D40")
 
 # Plots ----------------------------------------------------------------------
 
-# Colour by record type (organic/mineral).
 p <- ggplot(scrs) + 
      geom_point(mapping = aes(x=NMDS1,
                               y=NMDS2,
-                              color = record.type,
-                              size = total.amplicons)) +
+                              color = horizon,
+                              size = perul)) +
+     scale_color_manual(values = cb) +
      coord_fixed() +
      geom_segment(data = adjsig,
                   aes(x = 0, xend=NMDS1,
                       y = 0, yend = NMDS2),
                   arrow = arrow(length = unit(0.25, "cm")),
                   colour = "grey") + 
-     geom_text_repel(data = adjsig,
-                     aes(x=NMDS1, 
-                         y = NMDS2,
-                         label = Species),
+     geom_text(data = adjsig,
+                     aes(x=NMDS1, y = NMDS2, label = SPecies.sensible,
+                         angle = (180/pi) * atan(NMDS2/NMDS1)),
                      size = 3)
 p
 
-ggsave("all_significant_variables_NMDS-by-size.svg",
-       dpi=600,
-       width = 14,
-       height=9)
 
-# Colour by horizon.
-print(levels(scrs$horizon))
-scrs$horizon <- factor(scrs$horizon, levels(scrs$horizon)[c(3, 4, 2, 5, 1)])
-
-p <- ggplot(scrs) + 
-     geom_point(mapping = aes(x=NMDS1,
-                              y=NMDS2,
-                              color = horizon,
-                              size = total.amplicons)) + 
-     coord_fixed() + 
-     geom_segment(data = adjsig,
-                  aes(x=0,
-                      xend=NMDS1,
-                      y=0,
-                      yend = NMDS2),
-                      arrow = arrow(length = unit(0.25, "cm")), colour = "grey") + 
-     geom_text_repel(data = adjsig,
-                     aes(x=NMDS1,
-                         y = NMDS2,
-                         label = Species),
-                     size = 3)
-p
-
-ggsave("Horizon-by-size.svg",
-        dpi=600,
-        width = 14,
-        height=9)
+ggsave("NSIS_NDMS_all_significant_variables_post_BH2.svg",
+       dpi=1200)
