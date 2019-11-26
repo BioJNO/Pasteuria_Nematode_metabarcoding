acaaa8fccc218d9f2906a807701089e6eb6df1a0
BioJNO <JamieNeilOrr@gmail.com>
2019-11-26 10:40:40 +0000

    Removing old scripts

--- nematode-ZOTU-table/D.usearotutab.sh
+++
@@ -1,8 +0,0 @@
-cd /home/jo42324/metabarcode/analysis/sequence-analysis/Pasteuria_Nematode_metabarcoding/nematode-ZOTU-table
-
-# Map fq files to zotus to generate a ZOTU table.
-usearch -otutab all-nem-fq-w-usearch-sampleids.fastq \
--otus nematode_alpha1_zotus.fasta \
--otutabout nematode_alpha1_zotutab.txt \
--mapout nematode_alpha1_zotu_map.txt \
--notmatched nematode_alpha1_unmapped.fasta
